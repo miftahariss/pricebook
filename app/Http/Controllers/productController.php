f385db8e44823631ab8f7bd8c9c5fd65b80e9733
Miftah Aris Setiawan <miftahariss15@gmail.com>
2020-07-11 13:17:12 -0700

    add project

---
+++ app/Http/Controllers/productController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class productController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
