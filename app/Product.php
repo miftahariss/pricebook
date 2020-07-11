acc51db2ffa5576cc9676a4f70e3f5c2f2c75fdb
Miftah Aris Setiawan <miftahariss15@gmail.com>
2020-07-11 13:22:19 -0700

    add model product

---
+++ app/Product.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+   protected $table = 'product'; //nama table yang kita buat lewat migration adalah todo
+}
