72e65961550f41b8101787b4e013ed7b5612656b
Miftah Aris Setiawan <miftahariss15@gmail.com>
2020-07-11 13:21:33 -0700

    add product controller

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Validator;
+use App\Product;
+
 class productController extends Controller
 {
     /**
@@ -14,5 +18,39 @@ class productController extends Controller
         //
     }
 
-    //
+    public function index(){
+        $data = Product::all();
+
+        return response()->json([
+            'products' => $data,
+        ], 200);
+    }
+    public function show($id){
+        $data = Product::where('id',$id)->get();
+        
+        return response()->json([
+            'products' => $data,
+        ], 200);
+    }
+    public function store (Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Invalid Request! name and description is required!',
+            ], 401);
+        }
+
+        $data = new Product();
+        $data->name = $request->input('name');
+        $data->description = $request->input('description');
+        $data->save();
+
+        return response()->json([
+            'message' => 'Product data CREATED successfully!',
+        ], 200);
+    }
 }
