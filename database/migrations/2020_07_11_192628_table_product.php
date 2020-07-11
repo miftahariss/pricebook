903e5a57aef6d31c5eb7006b3a8ebfa47662aafe
Miftah Aris Setiawan <miftahariss15@gmail.com>
2020-07-11 13:33:06 -0700

    remove description field

--- database/migrations/2020_07_11_192628_table_product.php
+++ database/migrations/2020_07_11_192628_table_product.php
@@ -16,7 +16,6 @@ class TableProduct extends Migration
         Schema::create('product', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 200);
-            $table->text('description');
             $table->timestamps();
         });
     }
